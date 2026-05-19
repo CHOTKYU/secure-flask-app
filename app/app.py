7ae2295e87f44b8f24a716e50abd4c7179007293
CHOTKYU <shepyak03y@gmail.com>
2026-05-19 10:38:15 +0000

    update: app.py

--- app/app.py
+++ app/app.py
@@ -10,5 +10,10 @@ def home():
 def health():
     return jsonify({"status": "healthy"})
 
+@app.route('/version')
+def version():
+    return jsonify({"version": "1.0.0", "author": "Arseniy"})
+
+
 if __name__=='__main__':
   app.run(host='0.0.0.0', port=5000)
